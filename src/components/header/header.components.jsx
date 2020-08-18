9482630a46dfeb6fecaac4fab4db212536df33fd
ipinmi <ipinmiadebayo@gmail.com>
2020-08-18 11:58:30 +0100

    added carticon and cartdropdown components also a cart reducer

--- src/components/header/header.components.jsx
+++ src/components/header/header.components.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase';
+
+import CartIcon from '../cart-icon/cart-icon.components';
+import CartDropdown from '../cart-dropdown/cart-dropdown.components';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
-         <Link to="/">
-              <Logo className='logo' />
-         </Link>
+        <Link to="/">
+            <Logo className='logo' />
+        </Link>
 
-         <div className='options'>
-             <Link className='option' to='/shop'>
-                 SHOP
-             </Link>
-             <Link className='option' to='/shop'>
-                 CONTACT
-             </Link>
-
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                SHOP
+            </Link>
+            <Link className='option' to='/shop'>
+                CONTACT
+            </Link>
+            {/* redirecting */}
             {
                 // ternary opeator for div=true & link = false
                 currentUser ?
@@ -27,13 +31,21 @@ const Header = ({ currentUser }) => (
                 :
                 (<Link className='option' to='/signin'>SIGN IN</Link>)
             }
-         </div>
+
+            <CartIcon />
+
+        </div>
+        {
+            hidden ? null : <CartDropdown /> 
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
-})
+// destructuring the state to get specific values
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } })  => ({
+    currentUser,
+    hidden,
+});
 
 
 export default connect(mapStateToProps)(Header);
