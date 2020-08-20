9a72812dc8a04db07720ee3b204e25a5baeec969
ipinmi <ipinmiadebayo@gmail.com>
2020-08-20 15:44:27 +0100

    added redux persist for local storage cartItems

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// local storage object on our window browser as default storage
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user-reducer';
 import cartReducer from './cart/cart.reducer';
-export default combineReducers({
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    // array containing the string name of reducers we want to store
+    whitelist: ['cart']
+}
+
+const rootReducers = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig ,rootReducers);
